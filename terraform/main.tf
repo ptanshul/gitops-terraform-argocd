2865dd8456559719c855e20a26555cd0ab2fc4d9
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-14 17:47:22 +0530

    Initial Terraform project

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-gitops-terraform-argocd"
+  location = "Central India"
+}
