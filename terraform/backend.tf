fd75082557f388d4af867bbde5345c620fc00d6b
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-14 17:59:13 +0530

    Add Azure remote Terraform backend

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    use_oidc             = true
+    resource_group_name  = "developer-rg"
+    storage_account_name = "tfstategitops20260813"
+    container_name       = "tfstate"
+    key                  = "gitops-terraform.tfstate"
+  }
+}
