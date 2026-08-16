d17f9789cfac2586127e184dddd94ebed0841320
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-16 11:28:52 +0530

    Create v6 resource group through GitOps

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-gitops-terraform-argocd-v5"
+  name     = "rg-gitops-terraform-argocd-v6"
   location = "Central India"
 
   tags = {
