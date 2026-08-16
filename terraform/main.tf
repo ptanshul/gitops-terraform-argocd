52a9156966453536cb9f5bb043660e3293685420
ptanshul <anshul.tiwari@rediffmail.com>
2026-08-16 10:23:44 +0530

    Create v4 resource group through GitOps

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-gitops-terraform-argocd-v3"
+  name     = "rg-gitops-terraform-argocd-v4"
   location = "Central India"
 
   tags = {
